f924fd80c13c88c118250f0344f1bbb229f63840
Mihika Saxena <mihikasaxena13@gmail.com>
2024-06-25 15:22:18 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Display numbers from 2 to 100 with a gap of 2
+
+namespace ConsoleApp1
+{
+  internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Display table of 5
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("5 * " + i + " = " + (5 * i));
+            }
+        }
+
+
+    }
+}
